31b0babe46556717e5a981742b2c9077252ce6cf
이민성 <minmin@minbook.local>
2025-06-23 14:30:20 +0900

    Fin : 빌드 완료

--- swpp_team03/Assets/Scripts/EnergyItem.cs
+++ swpp_team03/Assets/Scripts/EnergyItem.cs
@@ -8,7 +8,7 @@ public class EnergyItem : MonoBehaviour
         Debug.Log($"[충돌 감지] {other.name}"); // 로그 찍기
         Debug.Log($"[충돌 감지] {other.gameObject.tag}"); // 로그 찍기
 
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player_Child"))
         {
             Debug.Log("[플레이어 감지됨] 에너지 증가!");
 			EffectManager.Instance.PlayEnergyRecover(transform.position);
